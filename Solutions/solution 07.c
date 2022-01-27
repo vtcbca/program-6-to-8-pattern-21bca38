29e2d56df43c2bef56e50b8c30728e391ea2bbd5
21bca38 <96710755+21bca38@users.noreply.github.com>
2022-01-27 13:58:10 +0530

    Update solution 07.c

--- Solutions/solution 07.c
+++ Solutions/solution 07.c
@@ -5,10 +5,12 @@ date:27/12/2021*/
 #include<conio.h>
 void main()
 {
-int a,b;
+int a,b,space;
 clrscr();
 for(a=5; a>=0; a=a-2)
 	{
+          for (space=0;space<5-a;++space) 
+          printf("  ");
 		for(b=1; b<=a; b++)
 		{
 			printf("* ");
